9934f3320fb201e811ef2a0c89eb64d41e8eb126
Bill Bloebaum <bill.bloebaum@dialexa.com>
2023-12-20 11:18:19 -0800

    add day 5

---
+++ src/day5/day.5.ts
@@ -0,0 +1,20 @@
+function parseInput() {
+  const input = Deno.readTextFileSync("src/day5/input.txt");
+
+  const parsed = input
+    .split("\n");
+
+  return parsed;
+}
+
+function part1() {
+  const input = parseInput();
+
+  return 0;
+}
+
+function part2() {
+  const input = parseInput();
+
+  return 0;
+}
